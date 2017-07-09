5f83787d81587aca385aea7e145b80d2f8536ca8
Khvorenkov Daniil <shas-el@mail.ru>
2017-07-09 14:30:43 +0400

    Поправил HWT_09

--- HWT_09/Task01/ExtensionHelper.cs
+++ HWT_09/Task01/ExtensionHelper.cs
@@ -1,26 +1,34 @@
 ﻿namespace Task01
 {
+    using System.Collections.Generic;
+
     public static class ExtensionHelper
     {
-        public static int Sum(this int[] array)//todo pn а мы обобщенные типы не проходили чтоли?
+        public static int Sum(this IEnumerable<int> array)//todo pn а мы обобщенные типы не проходили чтоли?
         {
             var sum = 0;
 
-            foreach (int i in array)
+            if (array != null)
             {
-                sum += i;
+                foreach (int i in array)
+                {
+                    sum += i;
+                }
             }
 
             return sum;
         }
 
-        public static double Sum(this double[] array)
+        public static double Sum(this IEnumerable<double> array)
         {
             var sum = .0;
 
-            foreach (double d in array)
+            if (array != null)
             {
-                sum += d;
+                foreach (int i in array)
+                {
+                    sum += i;
+                }
             }
 
             return sum;
--- HWT_09/Task02/Program.cs
+++ HWT_09/Task02/Program.cs
@@ -18,11 +18,13 @@ namespace Task02
 
                 if (input.IsNaturalNumber())
                 {
-                    Console.WriteLine("{0} is a natural number", input);//todo pn "является ли строка положительным целым числом" в каком месте ты увидел "натуральное число"?
-				}
+                    Console.WriteLine("{0} is a non-negative integer", input);//todo pn "является ли строка положительным целым числом" в каком месте ты увидел "натуральное число"?
+                                                                        //Some definitions, including the standard ISO 80000-2, begin the natural numbers with 0, corresponding to the non-negative integers
+                                                                        //https://en.wikipedia.org/wiki/Natural_number
+                }
                 else
                 {
-                    Console.WriteLine("{0} isn't a natural number", input);
+                    Console.WriteLine("{0} isn't a non-negative integer", input);
                 }
 
                 Console.WriteLine("\nPress Esc to exit program; Press any other key to continue\n");
